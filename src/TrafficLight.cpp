26c8052f12aa924044f61f2a4b32c11b55512e66
tasmanx <m.kuncevicius@gmail.com>
2020-04-20 13:57:07 +0300

    Implement simulate function

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -42,7 +42,8 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // start cycleThroughPhases function in a thread using TraficObject class' the thread queue vector
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
