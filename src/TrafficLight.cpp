e03b450a1790f7c32713ae74943d8ae942d67e83
tasmanx <m.kuncevicius@gmail.com>
2020-04-20 14:23:50 +0300

    Implement send function

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -15,8 +15,10 @@ T MessageQueue<T>::receive()
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex>
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    // add a new message to the queue and send a notification
+    std::lock_guard<std::mutex> lck(_mutex);
+    _queue.push_back(std::move(msg));
+    _cond.notify_one();
 }
 
 /* Implementation of class "TrafficLight" */
